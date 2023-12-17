200cc24285e54d865611c9dbe6fc61fe26695dec
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2023-12-17 22:21:31 +0600

    database initiated with team data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -12,6 +13,16 @@ const data = require("./data/books.json");
 const miniBooks = require("./data/miniBooks.json");
 const reviews = require("./data/reviews.json");
 const team = require("./data/team.json");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sahqxic.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Peon server is running");
@@ -30,9 +41,23 @@ app.get("/customerReview", (req, res) => {
   res.send(reviews);
 });
 
-app.get("/ourTeam", (req, res) => {
-  res.send(team);
-});
+// app.get("/ourTeam", (req, res) => {
+//   res.send(team);
+// });
+
+async function run() {
+  try {
+    const teamCollection = client.db("peonDB").collection("team");
+
+    app.get("/ourTeam", async (req, res) => {
+      const query = {};
+      const result = await teamCollection.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Peon server is running on port: ${port}`);
